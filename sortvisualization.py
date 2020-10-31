e4b471447c3216754e6a5ed65904e4c5f7155e22
JC <paolot55@gmail.com>
2020-10-31 14:23:35 -0700

    bringing file up to date before major changes

--- sortvisualization.py
+++ sortvisualization.py
@@ -1,43 +1,72 @@
 import pygame
 from random import choices
 from random import randrange
+from random import randint
+
 pygame.init()
 
-###### initialize global values ##############
+######### initialize global values ##############
 screen_size = screen_width, screen_height = 1440, 810
 screen_background_color = 0, 0, 0 # black 
 screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
+pygame.display.set_caption('Sorting algorithm visualization')
 running = True
+keys = {pygame.K_1:2, pygame.K_2:4, pygame.K_3:8, pygame.K_4:16, pygame.K_5:24} 
+
+class Main:
+    """ Object to hold all global values and functions for handling screen drawing and key presses """
+
+    def __init__(self):
+        pass
+    def keypress(self):
+        pass
+    def updatescreen(self):
+        pass
+
 
 class Graph:
     """ a graph of bars of random value to be sorted """
 
     def __init__(self):
         self.bars = []
-        pass
+        self.issorted = False
 
-    def gen_array(self, size = (screen_width // 8)):
-        position = 0
-        for x in choices(range(1, screen_height - 10), k=size):
-            self.bars.append(Bar(x, position))
-            position += 1
+    def gen_array(self, size = (screen_width // 8), rand_colors = False):
+        self.clear_array()  # ensure an empty array when generating a new one
+        for i, x in enumerate(choices(range(1, screen_height - 10), k=size)):
+            self.bars.append(Bar(position = i, value = x))
+        if rand_colors == True:
+            for bar in self.bars:
+                bar.random_color()
 
     def clear_array(self):
-        bars = []
+        self.bars = []
+        self.issorted = False
     
-    def draw_bars(self):
-        pass
+    def draw_bars(self, screen_width=screen_width, screen_height=screen_height, rand_colors=False):
+        """ draws a bar graph from an input array of values on the screen """
+
+        barwidth = screen_width // len(self.bars)
+        x, y = 0, screen_height
+
+        for bar in self.bars:
+            if rand_colors == True:
+                if randint(1,100) <= 20:
+                    bar.random_color() 
+            pygame.draw.rect(screen, bar.color, [x, y, barwidth, -(bar.height)])
+            x += barwidth
 
     def sort(self, algorithm):
-        pass
+        algorithm(self)
+
 
 class Bar:
     """ a bar in Graph to be sorted """
 
-    def __init__(self, value=1, position=0, color=(255,255,255)):
-        self.value = value
+    def __init__(self, position = 0, value = 1, color = (255,255,255)):
         self.position = position
+        self.value = value
         self.color = color
         self.height = value
 
@@ -46,42 +75,56 @@ class Bar:
 
     def __repr__(self):
         return "position: {}, value: {}, color: {}".format(self.position, self.value, self.color)
+    
+    def random_color(self):
+        self.color = (randrange(256), randrange(256), randrange(256))
 
-def gen_array(size = (screen_width // 4)):
-    """ Return an array of random elements to be sorted by various sorting algorithms """
-    return tuple(choices(range(1, screen_height - 10), k=size))
+    def clear_color(self):
+        self.color = (255, 255, 255) # white
 
-def draw_bars(arr, width = screen_width, height = screen_height):
-    """ draws a bar graph from an input array of values on the screen """
-    barwidth = width // len(arr)
-    bar_color = (255, 255, 255)
-    x, y = 0, height
+    def __lt__(self, other):
+        return self.value < other.value
 
-    for val in arr:
-        barheight = val
-        random_color = (randrange(255), randrange(255), randrange(255))
-        pygame.draw.rect(screen, random_color, [x, y, barwidth, -(barheight)])
-        #pygame.draw.rect(screen, bar_color, [x, y, barwidth, -(barheight)])
-        x += barwidth
 
-def bubble_sort(arr):
-    """ bubble sorts a list object """
+def bubble_sort(g):
+    """ bubble sorts a graph object """
 
-    lst = list(arr[:])
+    lst = g.bars
     j = 0
+    r = 0
     while True:
         count = 0
         for i in range(0, len(lst)-j-1):
             if lst[i] > lst[i+1]:
                 lst[i], lst[i+1] = lst[i+1], lst[i]  
                 count += 1
+
+            if r == 4:
+                screen.fill(screen_background_color)
+                g.draw_bars()
+                pygame.display.flip()
+                r = 0
+            r += 1
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_0:
+                    return None
         j += 1
         if count == 0:
-            return lst
+            g.issorted = True
+            return None
+
+def pause():
+    ispaused = True
+    while ispaused == True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                ispaused = False
+        pygame.time.wait(100)
 
 # should define a main function and encapsolate everything in that. Init, main loop, cleanup
-array = gen_array()
-keys = {pygame.K_1:1, pygame.K_2:2, pygame.K_3:4, pygame.K_4:8, pygame.K_5:16} 
+graph = Graph()
+graph.gen_array(rand_colors=True)
 
 while running: 
 
@@ -90,12 +133,19 @@ while running:
             running = False
         # looks for keypresses and changes the amount of bars in the array to change the way it looks
         if event.type == pygame.KEYDOWN and event.key in keys:
-            array = gen_array(size = (screen_width // keys[event.key]))
+            graph.gen_array(size = (screen_width // keys[event.key]), rand_colors=True)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            pause()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_0:
+            #graph.sort()
+            graph.sort(bubble_sort)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_9:
+            pass
 
     screen.fill(screen_background_color)
-    draw_bars(array)
+    graph.draw_bars(rand_colors=True)
+    #graph.draw_bars()
     pygame.display.flip()
-
     clock.tick(6)
     
 pygame.quit()
